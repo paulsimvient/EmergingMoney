6a49ff06493f40551c4d0269beac350f0ff9b589
Paul Cummings <pcummin2@gmu.edu>
2013-04-21 17:07:24 -0400

    new UI and ability to change variables

---
+++ EmergingMoney.py
@@ -0,0 +1,319 @@
+'''
+Referee File
+Craig Brown, Paul Cummin, Vipin P. Veetil
+CSS 610
+Paper: Emergence of Money and Middlemen
+'''
+from __future__ import division
+import math
+import time
+import random
+from random import choice
+import collections
+import csv
+from operator import itemgetter
+import Agents as ag
+import matplotlib.pyplot as plt	# to enable plot of histogram
+import numpy as np
+import constants as c
+import Tkinter
+
+
+class EmergingMoney():
+    
+    def __init__(self, na = c.numOfAgents, ng = c.numOfGoods, nr = c.numofRounds, ss = c.sampleSize):
+                
+        #This list will be used to keep track of goods that agents recieve...
+        #..though it is not their consumption goods, i.e. goods agents...
+        #choose to carry for purpose of indirect trade
+        
+        c.numOfAgents = na
+        c.numOfGoods = ng 
+        c.numofRounds = nr 
+        c.sampleSize = ss
+        
+        self.listofMoney=[0]*(c.numOfGoods)
+        
+        #agent self.types
+        self.types=[]
+        
+        #array of all trades
+        self.allTrades=[]
+        
+        #array of goods traded
+        self.goodsTraded=[]
+        
+        # We generate agent1 list of agents
+        self.agentList = [ag.simpleAgents() for count in xrange(c.numOfAgents)]
+        
+        self.listofMoney=[0]*(c.numOfGoods)
+       
+        #initialize agent self.types
+        for i in range(0,c.numOfGoods):
+            for j in range(0, c.numOfGoods):
+                if i != j:
+                    nlist = [str(i),str(j),str(j)]
+                    self.types.append(nlist)
+    
+        # And we give each agents agent1 list of consumption good,..
+        # ...production good and carry good. Initially the production...
+        # ...good and carry good are the same
+        for agent in self.agentList: 
+            agent.goods= choice(self.types)  
+            
+
+    # this function defines one round of play
+    
+    def playRound(self, agent1,agent2):
+    
+        # this function give the value for agent "agent1" of trading with agents "agent2"
+    
+        def value_trade(agent1,agent2):
+    
+            # if the good "agent2" has is "agent1" consumption good then "agent1" value...
+            # ...from trading is utility of 1 from consumption minus cost of trade
+    
+            if agent1.goods[c.consumed_good]==agent2.goods[c.held_good]:
+                tradeValue_Eat=1-(agent1.cost[int(agent2.goods[c.held_good])]+agent2.cost[int(agent1.goods[c.held_good])])/2
+                tradeValue=tradeValue_Eat
+                return tradeValue
+            else:
+    
+                #otherwise agent "agent1" computes the expected benefit from...
+                #... buying agent1 good it does not consume
+                
+                # first the agent computes the likelihood that it will be able...
+                #... to trade the good it recieves from "agent2" for the good it...
+                # consumes next period, to do this it simply looks at how many times..
+                #.. the good that "agent2" has been traded in the past
+    
+                if c.memory<len(self.goodsTraded):
+                    memory_goodTraded=self.goodsTraded[-c.memory:]
+                else:
+                    memory_goodTraded=self.goodsTraded
+    
+                past_trades=memory_goodTraded.count(agent2.goods[c.held_good])
+    
+                total_trades=len(self.goodsTraded)
+    
+    
+                # the agent then computes the expected cost of trading the good that..
+                #..."agent2" has next period, the agent simple takes the...
+                #..average cost from agent1 sample
+    
+                sampleList=random.sample(self.agentList,c.sampleSize)
+                sample_costList=[]
+                for i in sampleList:
+                    sample_costList.append(i.cost[int(agent2.goods[c.held_good])])
+                sampleCost=np.mean(sample_costList)
+    
+                # the agent also computes the cost of trading now
+    
+                costNow=(agent1.cost[int(agent2.goods[c.held_good])]+agent2.cost[int(agent1.goods[c.held_good])])/2
+                expected_costPartner=sampleCost
+                expected_costTomorrow=(agent1.cost[0]+expected_costPartner)/2
+    
+                # if the good "agent2" has has never been traded before then "agent1" assigns...
+                #... agent1 random value for aquiring it and subtracts the cost of trading
+    
+                if  past_trades==0:
+                    tradeValue=random.uniform(0,1)-costNow
+                    return tradeValue
+    
+                # if the good "agent2" has has been traded before, then agent1 computes...
+                #... the probability of trading it next period, computed expected...
+                #... cost of trading it, and also substracts cost of trading it now
+    
+                else:
+                    prob=past_trades/total_trades
+                    tradeValue_NoEat=prob*(1-expected_costTomorrow)-costNow
+                    tradeValue=tradeValue_NoEat
+                    return 1
+                    return tradeValue
+    
+    
+    
+        #this function computes the value from not trading and is...
+        #... indentical to the part of value_trade function after else...
+        #... statement, except that if "agent1" does not trade now then it does...
+        #... not incur the cost of trading now
+    
+        def value_noTrade(agent1,agent2):
+    
+                sampleList=random.sample(self.agentList,c.sampleSize)
+                sample_costList=[]
+                for i in sampleList:
+                    sample_costList.append(i.cost[int(agent2.goods[c.held_good])])
+                sampleCost=np.mean(sample_costList)
+                expected_costPartner=sampleCost
+    
+                expected_costTomorrow=(agent1.cost[0]+expected_costPartner)/2
+    
+                past_trades=self.goodsTraded.count(agent1.goods[c.held_good])
+                total_trades=len(self.goodsTraded)
+    
+                if past_trades==0:
+                    noTrade_value=random.uniform(0,1)
+                    return noTrade_value
+    
+                else:
+                    prob=past_trades/total_trades
+                    noTrade_value=prob*(1-expected_costTomorrow)
+                    return noTrade_value
+    
+        # this function computes whether agent "agent1" prefers to trade or not ...
+        #...trade, it returns true if agent "agent1" prefers to trade
+        def single_decision(agent1,agent2):
+            if value_trade(agent1,agent2)>value_noTrade(agent1,agent2):
+                return True
+            return False
+    
+    
+        # this function computes whether both agents wish to trade, ...
+        #...it returns true if both agents wish to trade
+        def final_decision(agent1,agent2):
+            if single_decision(agent1,agent2)==True and single_decision(agent2,agent1)==True:
+                return True
+            return False
+    
+    
+        # if both agents wish to trade then we update values, store data
+    
+        if final_decision(agent1,agent2)==True:
+    
+            # if the good one agent carries is the other agents consumption good..
+            #.. and vice versa
+    
+            if  agent1.goods[c.consumed_good]==agent2.goods[c.held_good] and agent1.goods[c.held_good]==agent2.goods[c.consumed_good]:
+    
+                #append the list of traded goods, which keeps track of couples trades
+
+                self.allTrades.append((agent1.goods[c.held_good],agent2.goods[c.held_good]))
+    
+                # append the list of traded goods which keeps track of...
+                #...individual goods traded
+    
+                self.goodsTraded.append(agent1.goods[c.held_good])
+                self.goodsTraded.append(agent2.goods[c.held_good])
+    
+                # agent agent1 and agent2 will update its data
+    
+                agent1.result(agent1.goods[c.consumed_good],
+                              agent2.goods[c.held_good],
+                              agent1.goods[c.held_good],
+                              value_trade(agent1,agent2))
+                
+                agent2.result(agent2.goods[c.consumed_good],
+                              agent1.goods[c.held_good],
+                              agent2.goods[c.held_good],
+                              value_trade(agent2,agent1))
+    
+    
+            elif agent1.goods[c.consumed_good]==agent2.goods[c.held_good] and agent1.goods[c.held_good]!=agent2.goods[c.consumed_good]:
+                
+                self.allTrades.append((agent1.goods[c.held_good],
+                                  agent2.goods[c.held_good]))
+                
+                self.goodsTraded.append(agent1.goods[c.held_good])
+                self.goodsTraded.append(agent2.goods[c.held_good])
+                
+                agent1.result(agent1.goods[c.consumed_good],
+                              agent2.goods[c.held_good],
+                              agent1.goods[c.held_good],
+                              value_trade(agent1,agent2))
+                
+                agent2.result(agent1.goods[c.held_good],
+                              agent1.goods[c.held_good],
+                              agent2.goods[c.held_good],
+                              value_trade(agent2,agent1))
+    
+                # since agent "agent2" is recieving agent1 good which is not its...
+                #... consumption good, we call this the use of money.
+    
+                self.listofMoney[int(agent1.goods[c.held_good])]+=1
+    
+            elif agent1.goods[c.consumed_good]!=agent2.goods[c.held_good] and agent1.goods[c.held_good]==agent2.goods[c.consumed_good]:
+                self.allTrades.append((agent1.goods[c.held_good],agent2.goods[c.held_good]))
+                self.goodsTraded.append(agent1.goods[c.held_good])
+                self.goodsTraded.append(agent2.goods[c.held_good])
+                
+                agent1.result(agent2.goods[c.held_good],
+                              agent2.goods[c.held_good],
+                              agent1.goods[c.held_good],
+                              value_trade(agent1,agent2))
+                
+                agent2.result(agent2.goods[c.consumed_good],
+                              agent1.goods[c.held_good],
+                              agent2.goods[c.held_good],
+                              value_trade(agent2,agent1))
+                
+                self.listofMoney[int(agent2.goods[c.held_good])]+=1
+    
+            elif agent1.goods[c.consumed_good]!=agent2.goods[c.held_good] and agent1.goods[c.held_good]!=agent2.goods[c.consumed_good]:
+                self.allTrades.append((agent1.goods[c.held_good],agent2.goods[c.held_good]))
+                
+                self.goodsTraded.append(agent1.goods[c.held_good])
+                self.goodsTraded.append(agent2.goods[c.held_good])
+                
+                agent1.result(agent2.goods[c.held_good],
+                              agent2.goods[c.held_good],
+                              agent1.goods[c.held_good],
+                              value_trade(agent1,agent2))
+                
+                agent2.result(agent1.goods[c.held_good],
+                              agent1.goods[c.held_good],
+                              agent2.goods[c.held_good],
+                              value_trade(agent2,agent1))
+    
+    
+                self.listofMoney[int(agent1.goods[c.held_good])]+=1
+                self.listofMoney[int(agent2.goods[c.held_good])]+=1
+
+
+
+    def playGame(self):
+         
+         for i in range(c.numofRounds):
+             
+            #randomly pick two agents from the list of agents
+            players=random.sample(self.agentList,2)
+            
+            p1=players[0]
+            p2=players[1]
+    
+            # ask the two players to play agent1 round of the game
+            self.playRound(p1,p2)
+        
+    def get_goods_money(self): 
+        #collect data
+        list_goods=list(xrange(c.numOfGoods))
+        strList_goods=[]
+        for i in list_goods:
+            strList_goods.append(str(i))
+        freq_goods=[]
+    
+        for i in strList_goods:
+            f=self.goodsTraded.count(i)
+            freq_goods.append(f)  
+            
+        return list_goods, self.listofMoney
+        
+    def plot(self):
+         
+        goods = self.get_goods_money()
+        
+        plt.scatter(goods[0],goods[1])
+    
+        buf = "%d Good Economy - %d rounds" % (c.numOfGoods, c.numofRounds)
+        plt.title(buf)
+        plt.xlabel("Goods")
+        plt.ylabel("Number of times a good is used as money")
+        plt.show()    
+
+
+def run():
+    em = EmergingMoney()
+    em.playGame()
+    em.plot()
+    
+ 
--- GUI.py
+++ GUI.py
@@ -2,15 +2,15 @@
 import matplotlib
 from random import *
 from Tkinter import *
-from Referee import *
+from EmergingMoney import *
+import copy
+
 matplotlib.use('TkAgg')
 
 from numpy import arange, sin, pi
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
 from matplotlib.figure import Figure
-
-
-
+ 
 import sys
 if sys.version_info[0] < 3:
     import Tkinter as Tk
@@ -19,69 +19,113 @@ else:
 
 def destroy(e): sys.exit()
 
+em = None
+slist = []
+sort = False
+   
 class Indicator:
     def __init__(self, master=None, label='', value=0.0, f = 0, t = 100):
         self.var = DoubleVar()
         self.s = Scale(master, 
                  label=label, 
                  variable=self.var,
-               from_=f, 
-               to=t, 
-               orient=HORIZONTAL,
-               length=300)
+                 from_=f, 
+                 to=t, 
+                 orient=HORIZONTAL,
+                 length=300)
         self.var.set(value)
-        self.s.pack()
+        self.s.pack() 
   
-def setTemp():
-    slider = choice(range(10))
-    value  = choice(range(0, 10))
-    slist[slider].var.set(value)
-    root.after(5, setTemp)
-        
-        
-root = Tk.Tk()
-root.wm_title("Emergence of Money")
+  
  
-f = Figure(figsize=(5,4), dpi=100)
+def init_plot():        
+    
+    # a tk.DrawingArea
+    canvas.show()
+    canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+
+    #toolbar = NavigationToolbar2TkAgg( canvas, root )
+    #toolbar.update()
+    canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
+     
+  
+    slist.append(Indicator(master=root, label='Number of Agents', value=500, f = 0, t = 1000)) 
+    slist.append(Indicator(master=root, label='Number of Goods', value=5, f = 0, t = 100)) 
+    slist.append(Indicator(master=root, label='Number of Rounds', value=1000, f = 0, t = 10000)) 
+    slist.append(Indicator(master=root, label='Sample Size', value=5, f = 0, t = 100)) 
+   
+ 
+def visualize():
+   
+    global em
+    
+    a.clear()
+    
+    #a.plot(t,s)
+    a.set_title('Emergence of Money')
+    a.set_xlabel('Goods')
+    a.set_ylabel('Goods Used As Money')
+ 
+    goods = em.get_goods_money()
+    
+    #sorted list
+    list_goods = copy.deepcopy(goods[1])
+    if sort == True:
+        list_goods.sort(reverse=True)
+     
+    a.scatter(goods[0],list_goods)    
+    canvas.show()
+    
+
+def sortAscend():
+    
+    global sort
+    sort = not sort
+    visualize()
+     
+ 
+def setCallback():
+    
+    global em
+    
+    #na = c.numOfAgents, ng = c.numOfGoods, nr = c.numofRounds, ss = c.sampleSize
+    v_0 = int(slist[0].var.get())
+    v_1 = int(slist[1].var.get())
+    v_2 = int(slist[2].var.get())
+    v_3 = int(slist[3].var.get())    
+    em = EmergingMoney(v_0, v_1, v_2, v_3)    
+    em.playGame()    
+    visualize()
+   
+
+#do everything
+root = Tk.Tk()
+root.wm_title("Emergence of Money")    
+
+#figure and canvas
+f = Figure(figsize=(6,5), dpi=100)
+canvas = FigureCanvasTkAgg(f, master=root) 
 a = f.add_subplot(111)
-t = arange(0.0,3.0,0.01)
-s = sin(2*pi*t)
-
-a.plot(t,s)
-a.set_title('Emergence of Money')
-a.set_xlabel('X axis label')
-a.set_ylabel('Y label')
-
-
-# a tk.DrawingArea
-canvas = FigureCanvasTkAgg(f, master=root)
+ 
 canvas.show()
 canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
-#toolbar = NavigationToolbar2TkAgg( canvas, root )
-#toolbar.update()
-canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
- 
-slist = []
-slist.append(Indicator(master=root, label='Goods')) 
-
-
-def setCallback():
-   print "setCallback" 
-   
 button = Tk.Button(master=root, text='Set', command = setCallback)
-button.pack(side=Tk.LEFT)
-
-button = Tk.Button(master=root, text='Quit', command=sys.exit)
 button.pack(side=Tk.RIGHT)
+canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)    
+button = Tk.Button(master=root, text='Quit', command=sys.exit)
+button.pack(side=Tk.RIGHT)   
 
- 
+c = Checkbutton(master=root, text="Sort Descending", command = sortAscend)
+c.pack(side=Tk.RIGHT)
 
-lists = playGame()
-print lists
 
+init_plot()
+
+em = EmergingMoney()
+em.playGame()
+visualize()
+  
 Tk.mainloop()
 
 
-
- 
