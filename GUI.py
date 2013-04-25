4bbc3b2a7aae8275b7dcfcf92e217a56eeaa6366
Paul Cummings <pcummin2@gmu.edu>
2013-04-25 19:04:19 -0400

    updates to GUI file

--- GUI.py
+++ GUI.py
@@ -21,7 +21,7 @@ def destroy(e): sys.exit()
 
 em = None
 slist = []
-sort = False
+sort = False 
    
 class Indicator:
     def __init__(self, master=None, label='', value=0.0, f = 0, t = 100):
@@ -48,13 +48,14 @@ def init_plot():
     #toolbar.update()
     canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
      
-  
+    slist.append(Indicator(master=root, label='Number Runs', value=1, f = 1, t = 1000))   
     slist.append(Indicator(master=root, label='Number of Agents', value=500, f = 0, t = 1000)) 
     slist.append(Indicator(master=root, label='Number of Goods', value=5, f = 0, t = 100)) 
-    slist.append(Indicator(master=root, label='Number of Rounds', value=1000, f = 0, t = 10000)) 
+    slist.append(Indicator(master=root, label='Number of Rounds (per run)', value=1000, f = 0, t = 10000)) 
     slist.append(Indicator(master=root, label='Sample Size', value=5, f = 0, t = 100)) 
    
- 
+   
+
 def visualize():
    
     global em
@@ -89,13 +90,17 @@ def setCallback():
     global em
     
     #na = c.numOfAgents, ng = c.numOfGoods, nr = c.numofRounds, ss = c.sampleSize
-    v_0 = int(slist[0].var.get())
-    v_1 = int(slist[1].var.get())
-    v_2 = int(slist[2].var.get())
-    v_3 = int(slist[3].var.get())    
-    em = EmergingMoney(v_0, v_1, v_2, v_3)    
-    em.playGame()    
-    visualize()
+    v_0 = int(slist[1].var.get())
+    v_1 = int(slist[2].var.get())
+    v_2 = int(slist[3].var.get())
+    v_3 = int(slist[4].var.get())    
+    
+    numRuns = int(slist[0].var.get())
+    
+    for i in range(0,numRuns):
+        em = EmergingMoney(v_0, v_1, v_2, v_3)    
+        em.playGame()    
+        visualize()
    
 
 #do everything
@@ -103,7 +108,7 @@ root = Tk.Tk()
 root.wm_title("Emergence of Money")    
 
 #figure and canvas
-f = Figure(figsize=(6,5), dpi=100)
+f = Figure(figsize=(6,4), dpi=100)
 canvas = FigureCanvasTkAgg(f, master=root) 
 a = f.add_subplot(111)
  
@@ -112,14 +117,16 @@ canvas.get_tk_widget().pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)
 
 button = Tk.Button(master=root, text='Set', command = setCallback)
 button.pack(side=Tk.RIGHT)
+
 canvas._tkcanvas.pack(side=Tk.TOP, fill=Tk.BOTH, expand=1)    
 button = Tk.Button(master=root, text='Quit', command=sys.exit)
 button.pack(side=Tk.RIGHT)   
 
 c = Checkbutton(master=root, text="Sort Descending", command = sortAscend)
-c.pack(side=Tk.RIGHT)
-
-
+c.pack()
+c = Checkbutton(master=root, text="Output", command = None)
+c.pack()
+ 
 init_plot()
 
 em = EmergingMoney()
